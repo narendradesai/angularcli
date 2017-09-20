d24795d8d19b602222a5c5399b3d6d19d3108c74
Narendra Desai <narendra_desai@persistent.co.in>
2017-09-20 15:20:04 +0530

    Full USER component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,11 +9,9 @@ export class UserComponent implements OnInit {
   name:string ="Narendra Desai";
   age:number;
   email:string;
-  address:{
-    street:string,
-    city:string,
-    state:string
-  }
+  address:Address;
+  hobbies : string[];
+  hello : any;
 
   constructor() { 
     console.log("Constructor ran..")
@@ -23,13 +21,43 @@ export class UserComponent implements OnInit {
     console.log("ngInit ran..");
     this.name="Narendra Shenvi Desai";
     this.age=55;
+    this.email = "shenvidesainarendra@gmail.com";
     this.address = {
       street:"Virnoda",
       city : "Pernem",
       state : "Goa"
     }
+    this.hobbies = ["Codding","Play Cricket","Travelling"];
+    this.hello = "Hello";
+  }
+
+  onClick(){
+    this.name="Narendra Desai";
+    this.hobbies.push("New Hobby");
+    //console.log("Hello");
+  }
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+  deleteHobby(hobby){
+    for(let i=0; i<this.hobbies.length; i++){
+      if(this.hobbies[i]==hobby){
+        this.hobbies.splice(i, 1);
+      }
+    }
+
+
+    console.log(hobby);
   }
 
 
+}
+
+interface Address{
+    street:string,
+    city:string,
+    state:string
 }
 
